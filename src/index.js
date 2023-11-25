e1b24dfa93690dec9d880cc364899308a5ee0e14
Nadin2611 <132609243+Nadin2611@users.noreply.github.com>
2023-11-25 14:28:08 +0200

    fixed

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import { GlobalStyle } from 'styles/GlobalStyles';
+import { theme } from 'styles/theme';
+import { ThemeProvider } from 'styled-components';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+    <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
